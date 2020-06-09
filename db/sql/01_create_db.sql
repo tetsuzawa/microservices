8e67b9c2a151b5d65c9a24cf55e9f5b169173825
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-06-09 09:13:58 +0900

    Update: スキーマにpostを追加

--- db/sql/01_create_db.sql
+++ db/sql/01_create_db.sql
@@ -1,24 +1,37 @@
 CREATE DATABASE IF NOT EXISTS microservices;
 
 CREATE TABLE IF NOT EXISTS microservices.users (
-    id VARCHAR(36) NOT NULL UNIQUE,
-    user_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    remember_token VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id              VARCHAR(36)  NOT NULL,
+    user_name       VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) UNIQUE NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    remember_token  VARCHAR(100),
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS microservices.hrtfs (
-    id VARCHAR(36) NOT NULL UNIQUE,
-    user_id VARCHAR(36) NOT NULL,
-    storage_id VARCHAR(255) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
-    license VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id          VARCHAR(36)  NOT NULL,
+    user_id     VARCHAR(36)  NOT NULL,
+    storage_id  VARCHAR(255) NOT NULL,
+    filename    VARCHAR(255) NOT NULL,
+    license     VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     CONSTRAINT fk_hrtf_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE posts NOT EXISTS microservices.posts (
+(
+    id             varchar(36)  NOT NULL,
+    user_id        varchar(36)  NOT NULL,
+    text           varchar(100) NOT NULL,
+    parent_post_id varchar(36),
+    comment_count  INT          NOT NULL DEFAULT 0,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_post_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
