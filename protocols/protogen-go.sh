002a79e6a618e1849d147e135219aa01aae355b2
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-06-09 09:24:43 +0900

    Add: postの生成を追加

--- protocols/protogen-go.sh
+++ protocols/protogen-go.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 protoc -I/usr/local/include/ -I./user/ user/user.proto --go_out=plugins=grpc:../backend/pkg/api/
 protoc -I/usr/local/include/ -I./hrtf/ hrtf/hrtf.proto --go_out=plugins=grpc:../backend/pkg/api/
+protoc -I/usr/local/include/ -I./post/ post/post.proto --go_out=plugins=grpc:../backend/pkg/api/
